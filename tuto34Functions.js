af1eb9760d905d98f9f88b3bbbc03806ee74efb4
sandip235 <43064930+sandip235@users.noreply.github.com>
2021-03-03 16:23:41 +0530

    03032021

---
+++ tuto34Functions.js
@@ -0,0 +1,18 @@
+console.log("Hello this is saparate javascript file");
+
+let name="abc";
+let name1="xyz";
+
+console.log(name+" is good boy");
+console.log(name1+" is good boy");
+
+let a1="abc";
+let a2="pqr";
+
+function greet(name)
+{
+    console.log(name+"function used");
+}
+
+greet(a1);
+greet(a2);
